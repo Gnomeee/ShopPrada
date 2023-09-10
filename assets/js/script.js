4072dfb7ff287c93668801b63a25c2d303325385
Gnomeee <78102313+Gnomeee@users.noreply.github.com>
2023-09-10 20:04:42 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-$('.icon-heart').on('click', function(e){
-    $(this).css('background-color', 'red');
-    return $(this).on('click', function() {
-        $(this).css('background-color', 'white');
-        return $(this).on('click', function() {
-            $(this).css('background-color', 'red');
-            return 
-        })
-        
-    })
- });
+let iconHeart = document.querySelectorAll(".icon-heart");
 
+for (let i = 0; i < iconHeart.length; i++) {
+  iconHeart[i].addEventListener("click", function () {
+    if (iconHeart[i].style.backgroundColor == "red") {
+      iconHeart[i].style.backgroundColor = "white";
+    } else {
+      iconHeart[i].style.backgroundColor = "red";
+    }
+  });
+}
